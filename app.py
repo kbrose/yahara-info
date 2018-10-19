af19e95985982d8d9ddad2d09144be3e3b033cee
Kevin Rose <kevin+gh@maypark.com>
2018-10-18 22:23:12 -0500

    Simple flask app

---
+++ app.py
@@ -0,0 +1,20 @@
+import flask
+import madison_lake_levels as mll
+
+app = flask.Flask(__name__)
+
+@app.route('/')
+def main():
+    current = mll.scrape.scrape()
+    required_levels = mll.required_levels.required_levels
+    lakes = ['mendota', 'monona', 'waubesa', 'kegonsa']
+    statuses = [
+        current[lake].iloc[0] > required_levels.loc[lake, 'summer_maximum']
+        for lake in lakes
+    ]
+    statuses = dict(zip([lake.title() for lake in lakes], statuses))
+    return flask.render_template('main.html', statuses=statuses)
+
+
+if __name__ == '__main__':
+    app.run()
--- madison_lake_levels/required_levels.py
+++ madison_lake_levels/required_levels.py
@@ -4,3 +4,4 @@ import pandas as pd
 
 _data_path = Path(__file__).parent / 'data'
 required_levels = pd.read_csv(_data_path / 'required_levels.csv')
+required_levels.set_index('lake', drop=True, inplace=True)
